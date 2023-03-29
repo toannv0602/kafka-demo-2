4da973d8e7d4d4bbdb857e306bd5db4c95af7db5
Ngo Van Toan <66518663+toannv0602@users.noreply.github.com>
2023-03-29 18:19:24 +0700

    add ip

--- src/main/java/com/example/kafkademo/configuration/KafkaConsumerConfig.java
+++ src/main/java/com/example/kafkademo/configuration/KafkaConsumerConfig.java
@@ -19,7 +19,7 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.100.168:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://192.168.100.186:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
